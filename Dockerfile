437ff577f350d89d560f1d0a977372da3ede51c5
Filip Czapliński <filip.czaplinski92@gmail.com>
2024-03-27 17:27:00 +0100

    Add mariadb container

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,16 @@
 FROM php:8.3-apache
 
+RUN apt-get update && apt-get install -y \
+    git \
+    curl \
+    libpng-dev \
+    libonig-dev \
+    libxml2-dev \
+    zip \
+    unzip
+
+RUN docker-php-ext-install gd pdo pdo_mysql sockets
+
 ADD . /var/www/html
 
 COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
--- index.php
+++ index.php
@@ -1,3 +1,24 @@
 <?php
 
-echo 'hello world!';
+echo 'hello world!'. PHP_EOL;
+
+$serverName = "mariadb";
+$username = "root";
+$password = "secret";
+$database = "app_db";
+
+try {
+    $conn = new PDO("mysql:host=".$serverName.";dbname=" . $database, $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = $conn->prepare("SELECT * FROM Test");
+    $sql->execute();
+
+    while($result = $sql->fetch(PDO::FETCH_ASSOC)){
+        echo $result['test_field'];
+    }
+
+}
+catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
